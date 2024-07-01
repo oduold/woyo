00876bc4fe987c13653d1eb73069fc27ae131dd8
Donald O <don.opiyo@robin.net>
2024-07-02 00:16:34 +0100

    feat(hello-world): how are you

---
+++ src/main/java/com/woyo/ai/WoyoAiApplication.java
@@ -0,0 +1,13 @@
+package com.woyo.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoyoAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoyoAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woyo/ai/WoyoAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woyo.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoyoAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
